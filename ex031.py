b671ea34849d5afccfe6582c7ae289cb73cbc560
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-05-30 11:50:36 -0300

    Update ex031

--- ex031.py
+++ ex031.py
@@ -1,7 +1,7 @@
 km = float(input('Digite quantos KM tem sua viagem: '))
 if km <= 200:
     preco = km * 0.50
-    print('Sua viagem ficou no valor de R$ {}!'.format(preco))
+    print('Sua viagem ficou no valor de R$ {:.2f}!'.format(preco))
 else:
     preco2 = km * 0.45
-    print('Sua viagem ficou no valor de R$ {}!'.format(preco2))
+    print('Sua viagem ficou no valor de R$ {:.2f}!'.format(preco2))
