558d3cd1adfa5c5f253b06143b0f9e1d2d988b75
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-02 12:13:05 -0300

    ex037
    incompleto

---
+++ ex037.py
@@ -0,0 +1,5 @@
+#bases numéricas
+numero = int(input('Digite um número inteiro qualquer: '))
+base = int(input('Quer converter para qual base?\n 1 -> para binário, 2 -> para octal, 3 -> para hexadecimal:  '))
+
+
