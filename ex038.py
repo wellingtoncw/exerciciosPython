412289d5d0e74f34496f9641208683320ed7b2ca
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-02 12:24:56 -0300

    ex038
    comparando numeros

---
+++ ex038.py
@@ -0,0 +1,9 @@
+# comparando qual é maior
+numero1 = int(input('Digite o primeiro valor: '))
+numero2 = int(input('Digite o segundo valor: '))
+if numero1 > numero2:
+    print('O PRIMEIRO valor é MAIOR!')
+elif numero1 < numero2:
+    print('O SEGUNDO valor é MAIOR')
+else:
+    print('Não existe valor MAIOR.. Os números são IGUAIS!')
