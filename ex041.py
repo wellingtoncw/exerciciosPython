60a2bef9c3cda4b07db1c1ceb55d22c17fa04a5d
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-03 18:06:02 -0300

    atualiza ex041

--- ex041.py
+++ ex041.py
@@ -4,13 +4,18 @@ nascimento = int(input('Digite seu ano de nascimento: '))
 ano = date.today().year
 idade = ano - nascimento
 if idade <= 9:
+    print(idade, 'anos')
     print('MIRIM!')
 elif idade > 9 and idade <= 14:
+    print(idade, 'anos')
     print('INFANTIL!')
 elif idade > 14 and idade <= 19:
+    print(idade, 'anos')
     print('JUNIOR!')
 elif idade > 19 and idade <= 20:
+    print(idade, 'anos')
     print('SÊNIOR!')
 else:
-    print('Extraterrestre!!')
+    print(idade, 'anos')
+    print('MASTER!')
 
