b5fd70acf0725912aae0f93243c9db3b6de1f14e
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-03 19:05:25 -0300

    atualiza ex045
    jokenpo

--- ex045.py
+++ ex045.py
@@ -1,8 +1,48 @@
 #jokenpo
 from random import randint
-jogador = int(input('Escolha entre (Pedra, Papel e Tesoura...\n 1 - Pedra, 2 - Papel, 3 - Tesoura: '))
-computador = randint(1, 3)
-if jogador == 1 and computador == 1 or jogador == 2 and computador == 2 or jogador == 3 and computador == 3:
-    print('VOCÊ GANHOU!')
-else:
-    print('VOCÊ PERDEU!')
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas Opções:
+[0 ] PEDRA
+[1] PAPEL
+[2] TESOURA''')
+jogador = int(input('Qual sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+sleep(1)
+print('-=-' * 11)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-=-' * 11)
+if computador == 0: #computador jogou PEDRA
+    if jogador == 0:
+        print('EMPATE!')
+    elif jogador == 1:
+        print('JOGADOR VENCE!')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE!')
+    else:
+        print('OPÇÃO INVÁLIDA! TENTE NOVAMENTE!')
+elif computador == 1: #computador jogou PAPEL
+    if jogador == 0:
+        print('COMPUTADOR VENCE!')
+    elif jogador == 1:
+        print('EMPATE!')
+    elif jogador == 2:
+        print('JOGADOR VENCE!')
+    else:
+        print('OPÇÃO INVÁLIDA! TENTE NOVAMENTE!')
+elif computador == 2: # computador jogou TESOURA
+    if jogador == 0:
+        print('JOGADOR VENCE!')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE!')
+    elif jogador == 2:
+        print('EMPATE!')
+    else:
+        print('OPÇÃO INVÁLIDA! TENTE NOVAMENTE!')
+
