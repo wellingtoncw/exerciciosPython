041703a5421b69688e9ceb14d2879f800d3b1169
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-03 17:55:21 -0300

    atualiza ex037

--- ex037.py
+++ ex037.py
@@ -1,5 +1,18 @@
 #bases numéricas
 numero = int(input('Digite um número inteiro qualquer: '))
-base = int(input('Quer converter para qual base?\n 1 -> para binário, 2 -> para octal, 3 -> para hexadecimal:  '))
+print('''Escolha uma das bases para conversão:
+[1] converter para BINÁRIO
+[2] converter para OCTAL
+[3] converter para HEXADECIMAL''')
+opcao = int(input('Sua opção: '))
+if opcao == 1:
+    print('O número {} convertido para BINÁRIO é igual a {}.'.format(numero, bin(numero)[2:])) # [2:] SERVE PRA FATIAMENTO. A STRING COMEÇA A PARTIR DA SEGUNDA POSIÇÃO
+elif opcao == 2:
+    print('O número {} convertido para OCTAL é igual a {}.'.format(numero, oct(numero)[2:]))
+elif opcao == 3:
+    print('O número {} convertido para HEXADECIMAL é igual a {}.'.format(numero, hex(numero)[2:]))
+else:
+    print('OPÇÃO INVÁLIDA!')
+
 
 
