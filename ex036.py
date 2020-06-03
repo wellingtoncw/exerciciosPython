7b8561b0969d18f78c9730895d5c8fe47743283d
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-03 17:38:23 -0300

    atualiza ex036
    atualiza calculo prestacao

--- ex036.py
+++ ex036.py
@@ -2,9 +2,9 @@
 valorcasa = float(input('Qual o valor da Casa R$: '))
 salario = float(input('Quanto você ganha por mês R$: '))
 anos = int(input('Deseja parcelar em quantos anos? '))
-prestacao = valorcasa / anos
+prestacao = valorcasa / (anos * 12)
 if prestacao <= (salario * 30/100):
-    print('Sua prestação será no valor de: {}, durante {} anos!'.format(prestacao, anos))
+    print('Sua prestação será no valor de: {:.2f}, durante {} anos!'.format(prestacao, anos))
 else:
     print('PROPOSTA NEGADA! O valor da sua prestação esta maior do que 30% de seu salário.\n Valor da Prestação: {:.2f}. 30% do Salário: {:.2f}.'.format(prestacao, (salario * 30/100)))
 
