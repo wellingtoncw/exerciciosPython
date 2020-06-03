45bdd027df6be0955873db5fc432711ed0643197
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-03 18:22:59 -0300

    atualiza ex042

--- ex042.py
+++ ex042.py
@@ -6,8 +6,12 @@ r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os segmentos acima PODEM FORMAR triângulos!')
-#elif r1 == r2 and r2 == r3:
- #       print('TRIÂNGULO EQUILÁTERO!')
+    print('Os segmentos acima PODEM FORMAR um triângulo ', end='')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO!')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO!')
+    else:
+        print('ISÓSCELES!')
 else:
     print('Os segmentos acima NÃO PODEM formar triângulos!')
