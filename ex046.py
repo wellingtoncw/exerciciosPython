f726eeb0cfb1f594b6da5565415937791e3ad281
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-04 17:07:43 -0300

    ex046
    contagem regressiva

---
+++ ex046.py
@@ -0,0 +1,9 @@
+#contagem regressiva
+from time import sleep
+print('-=-' * 15)
+print('CONTAGEM REGRESSIVA PARA FOGOS!!')
+print('-=-' * 15)
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+print('FELIZ ANO NOVO!!!')
