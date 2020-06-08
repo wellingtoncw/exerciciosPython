898565bf733879b5ffbaedbb9c25abde76b16173
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-08 10:32:47 -0300

    atualiza ex049
    comentario

--- ex049.py
+++ ex049.py
@@ -3,7 +3,7 @@ t = 0
 print('A TABUADA DO NÚMERO DIGITADO É: ')
 print('-' * 12)
 for c in range(0, 11):
-    t = numero * c
+    t = numero * c # a variavel t recebe a multiplicação do numero digitado e o contador
     print('{} x {:2} = {}'.format(numero, c, t))
 print('-' * 12)
 print('FIM!!!')
