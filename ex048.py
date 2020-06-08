bf20195ea01cec2bdf12e1a6dca991013ac565f2
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-08 10:20:42 -0300

    atualiza ex048
    soma dos impares multiplos de 3

--- ex048.py
+++ ex048.py
@@ -1,7 +1,7 @@
-# divisiveis por 3
+# soma dos impares multiplos de 3
 s = 0
-for c in range(1, 7):
-    if c % 3 == 0:
-       s += c
-print('A soma dos múltiplos de 3 entre 1 e 3 é de {}.'.format(s))
-    #print(c)
+for c in range(1, 501):
+  if c % 3 == 0 and c % 2 != 0:
+    s += c # soma = soma + c
+    print(c)
+print('A SOMA dos múltiplos de 3 entre 1 e 500 é de {}.'.format(s))
