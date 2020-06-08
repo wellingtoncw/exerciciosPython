500a688cb02e6c6df76873418ab9e6c4907fa4db
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-08 11:07:29 -0300

    atualiza

--- ex048.py
+++ ex048.py
@@ -1,7 +1,9 @@
 # soma dos impares multiplos de 3
 s = 0
+cont = 0
 for c in range(1, 501):
   if c % 3 == 0 and c % 2 != 0:
+    cont += 1
     s += c # soma = soma + c
     print(c)
-print('A SOMA dos múltiplos de 3 entre 1 e 500 é de {}.'.format(s))
+print('A SOMA dos {} valores múltiplos de 3 entre 1 e 500 é de {}.'.format(cont, s))
