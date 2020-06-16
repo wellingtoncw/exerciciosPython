c642d39f9274e6dc6cab241fcf32e9994666b1d4
wellingtoncw7 <wellingtoncw7@gmail.com>
2020-06-16 18:50:50 -0300

    ex061
    PA com while

---
+++ ex061.py
@@ -0,0 +1,12 @@
+print('Gerador de PA')
+print('-=-' * 10)
+primeiro = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razão: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')
+
